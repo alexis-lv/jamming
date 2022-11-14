6a9d96258f998a7dd029c43a27550325155ea4e3
AlexisWeiss <alexissxo143@gmail.com>
2022-11-14 10:18:31 -0800

    step 80 commit

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -8,6 +8,15 @@ const Spotify = {
 
         const accessTokenMatch = window.location.href.match(/access_token=([^&]*)/);
         const expiresInMatch = window.location.href.match(/expires_in=([^&]*)/);
+
+        if(accessTokenMatch && expiresInMatch) {
+            accessToken = accessTokenMatch[1]; 
+            const expiresIn = Number(expiresInMatch[1]);
+
+            window.setTimeout(() => accessToken = '', expiresIn * 1000);
+            window.history.pushState('Access Token', null, '/');
+            return accessToken;
+        }
     }
 
 }
