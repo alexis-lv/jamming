f5ca47490dbca261390d8dc5b17209a265c83ec6
AlexisWeiss <alexissxo143@gmail.com>
2022-11-13 19:58:34 -0800

    step 61 commit change playlist name working

--- src/Components/Playlist/Playlist.js
+++ src/Components/Playlist/Playlist.js
@@ -19,11 +19,12 @@ class Playlist extends React.Component {
     render() {
         return (
             <div className="Playlist">
-                <input defaultValue={"New Playlist"}/>
+                <input defaultValue={"New Playlist"} onChange={this.handleNameChange}/>
               {/*   <!-- Add a TrackList component --> */}
               <TrackList tracks={this.props.playlistTracks}
                         onRemove={this.props.onRemove}
-                        isRemoval={true}/>
+                        isRemoval={true}
+                        />
               
                 <button className="Playlist-save">SAVE TO SPOTIFY</button>
             </div>
